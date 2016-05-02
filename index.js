cf05dff8e47bb766feede76ca6fd054eacb86df6
Marie Godon <mariefrance.godon@gmail.com>
2016-05-02 18:11:09 +0000

    exercise 1, say hello world at /hello

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/', function(req, res) {
+    res.send('Hello World!');
+});
+
+app.get('/hello', function(req, res) {
+    res.send('<h1>Hello World!</h1>')
 });
 
 
@@ -12,9 +16,9 @@ app.get('/', function (req, res) {
 /* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */
 
 // Boilerplate code to start up the web server
-var server = app.listen(process.env.PORT, process.env.IP, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+var server = app.listen(process.env.PORT, process.env.IP, function() {
+    var host = server.address().address;
+    var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+    console.log('Example app listening at http://%s:%s', host, port);
 });
